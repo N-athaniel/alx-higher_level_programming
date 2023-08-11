68688f741fd782bda4124310d4190c37025aae18
N-athaniel <ndoh1996@gmail.com>
2023-08-10 17:51:42 -0700

    a program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from variable_load_5 import a
+
+def find_var():
+    print(a)
 
 if __name == "__main__":
-    """print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-
-    print(a)
+    find_var()
